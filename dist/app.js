5427d2fc4cd30f51b8901182a88ec821016629a6
Santiago Ayala <santitomayala07@gmail.com>
2025-12-29 11:08:47 -0300

    Archivos de transpilacion de ts a js

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+//* Importaciones
+import express from "express";
+import { diariesRouter } from "./routes/diaries.routes.js";
+const app = express();
+const PORT = 4000;
+//* Middlewares
+app.use(express.json());
+//* endpoints
+app.get("/ping", (_req, res) => {
+    console.log("funcionando");
+    res.send("pong");
+});
+app.use("/api/diaries", diariesRouter);
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+});
---
+++ dist/routes/diaries.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+export const diariesRouter = Router();
+diariesRouter.get("/", (_req, res) => {
+    res.send("Finding all diaries");
+});
+diariesRouter.post("/", (_req, res) => {
+    res.send("Saving a diary");
+});
